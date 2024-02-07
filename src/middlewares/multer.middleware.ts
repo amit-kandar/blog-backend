cf8dede78ba93d43d8b5303ab85ab46035f04c74
Amit Kandar <kandaramit2001@gmail.com>
2024-02-07 21:32:50 +0530

    Update multer middleware

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -7,17 +7,7 @@ const storage = multer.diskStorage({
         cb(null, './public/temp');
     },
     filename: function (req: Request, file: Express.Multer.File, cb) {
-        // Access user-provided name from the request body or query params
-        const userProvidedName = req.body.name || req.body.title;
-
-        // Generate a random 6-digit number
-        const randomSixDigits = Math.floor(100000 + Math.random() * 900000);
-
-        // Get the file extension
-        const ext = path.extname(file.originalname);
-
-        // Construct the filename with user-provided name and random number
-        const fileName = `${userProvidedName}_${randomSixDigits}${ext}`;
+        const fileName = file.originalname;
 
         cb(null, fileName);
     }
